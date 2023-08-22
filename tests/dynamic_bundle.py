67f07df64e02cef99a061870c7ecc52d88a41d5b
hogura <heguangrongdd@qq.com>
2023-08-22 20:52:11 +0800

    Add naive add_bundle test

---
+++ tests/dynamic_bundle.py
@@ -0,0 +1,28 @@
+import ray
+from ray.util import placement_group
+from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
+
+n_node = 1
+
+@ray.remote(num_cpus=1)
+def gao():
+    print('hahaha')
+
+ray.init('auto')
+
+pg = placement_group(bundles=[{'cpu': 8.0}], strategy='SPREAD')
+
+ray.wait(pg.ready())
+
+t = [
+    gao.options(
+        scheduling_strategy=PlacementGroupSchedulingStrategy(
+            placement_group=pg,
+            placement_group_bundle_index=i
+        )
+    ) for i in range(n_node)
+]
+
+pg.add_bundles([{'cpu': 8.0}])
+
+ray.get(t)
